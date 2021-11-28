f3724f89e92e376c3352cd25218c4be339f4d42c
Alonso de Oliveira <alonsoboj@gmail.com>
2021-11-28 17:02:23 -0300

    Adição da solução do problema 1507.

--- Subsequências/Program.cs
+++ Subsequências/Program.cs
@@ -1,15 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1507
+
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Subsequências {
   internal class Program {
+    public static bool verificacao(string principal, string substring) {
+
+      int caracteresCorrespondentes = 0;
+      int indice = 0;
+
+      foreach (char caractere in principal) {
+        if (caractere == substring[indice]) {
+          caracteresCorrespondentes++;
+          indice++;
+
+          if (caracteresCorrespondentes == substring.Length) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
     static void Main(string[] args) {
 
+      StringBuilder saida = new StringBuilder();
 
+      int quantidadeDeTestes = int.Parse(Console.ReadLine());
 
+      for (int i = 0; i < quantidadeDeTestes; i++) {
+
+        string stringPrincipal = Console.ReadLine();
+        int quantidadeDeQueries = int.Parse(Console.ReadLine());
+
+        for (int j = 0; j < quantidadeDeQueries; j++) {
+
+          string substring = Console.ReadLine();
+
+          if (verificacao(stringPrincipal, substring)) {
+            saida.AppendLine("Yes");
+          }
+          else {
+            saida.AppendLine("No");
+          }
+        }
+      }
+      Console.Write(saida);
+      Console.ReadLine();
     }
   }
 }
