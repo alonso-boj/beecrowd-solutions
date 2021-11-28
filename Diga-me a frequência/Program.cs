2eac26fcefd8412a6808c4e5ab4651163751e4ff
Alonso de Oliveira <alonsoboj@gmail.com>
2021-11-28 12:09:44 -0300

    Tentativa de resolução do problema 1251.

---
+++ Diga-me a frequência/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diga_me_a_frequência {
+  internal class Program {
+    static void Main(string[] args) {
+
+      string entrada;
+
+      StringBuilder saida = new StringBuilder();
+
+      while (!string.IsNullOrEmpty(entrada = Console.ReadLine())) {
+
+        List<char> listaDeCaracteres = new List<char>();
+
+        listaDeCaracteres.AddRange(entrada);
+
+        Dictionary<char, int> dicionarioDePalavras = new Dictionary<char, int>();
+
+        foreach (char caractere in listaDeCaracteres) {
+
+          if (dicionarioDePalavras.ContainsKey(caractere)) {
+            dicionarioDePalavras[caractere]++;
+          }
+          else {
+            dicionarioDePalavras.Add(caractere, 1);
+          }
+        }
+
+        foreach (KeyValuePair<char, int> caractere in dicionarioDePalavras.OrderBy(d => d.Value)) {
+
+          saida.AppendLine($"{(int)caractere.Key} {caractere.Value}");
+        }
+        saida.AppendLine();
+      }
+      Console.Write(saida);
+      Console.ReadLine();
+    }
+  }
+
+}
