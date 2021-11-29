ce93e5cd184071b4df0ef40f98cb29877119790e
Alonso de Oliveira <alonsoboj@gmail.com>
2021-11-28 23:39:44 -0300

    Adição da solução do problema 1251.

--- Diga-me a frequência/Program.cs
+++ Diga-me a frequência/Program.cs
@@ -1,26 +1,41 @@
-﻿using System;
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1251
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-namespace Diga_me_a_frequência {
-  internal class Program {
+namespace _1251 {
+  class Program {
     static void Main(string[] args) {
 
-      string entrada;
-
-      StringBuilder saida = new StringBuilder();
+      string entrada = null;
+      var entradas = new List<string>();
 
       while (!string.IsNullOrEmpty(entrada = Console.ReadLine())) {
 
+        if (entrada.Length >= 1001)
+          continue;
+
+        entradas.Add(entrada);
+      }
+
+      bool line = false;
+
+      foreach (var input in entradas) {
+
+        if (line)
+
+        Console.Write(Environment.NewLine);
+        var formatted = input.Trim().Replace("\n", "").Replace("\r", "");
+        StringBuilder saida = new StringBuilder();
         List<char> listaDeCaracteres = new List<char>();
-
-        listaDeCaracteres.AddRange(entrada);
-
+        listaDeCaracteres.AddRange(formatted);
         Dictionary<char, int> dicionarioDePalavras = new Dictionary<char, int>();
 
         foreach (char caractere in listaDeCaracteres) {
 
+          var charInt = (int)caractere;
+
+          if (charInt <= 31 || charInt >= 128) continue;
           if (dicionarioDePalavras.ContainsKey(caractere)) {
             dicionarioDePalavras[caractere]++;
           }
@@ -28,16 +43,15 @@ namespace Diga_me_a_frequência {
             dicionarioDePalavras.Add(caractere, 1);
           }
         }
-
-        foreach (KeyValuePair<char, int> caractere in dicionarioDePalavras.OrderBy(d => d.Value)) {
-
+        foreach (KeyValuePair<char, int> caractere in dicionarioDePalavras.OrderBy(d => d.Value).ThenByDescending(d => d.Key)) {
           saida.AppendLine($"{(int)caractere.Key} {caractere.Value}");
         }
-        saida.AppendLine();
+        Console.Write(saida);
+
+        line = true;
+
+        Console.ReadLine();
       }
-      Console.Write(saida);
-      Console.ReadLine();
     }
   }
-
 }
